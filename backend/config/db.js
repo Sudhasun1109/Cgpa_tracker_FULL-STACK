7964feddc1b45385ab2a18c51dfaaef25818da0d
Sudhasun1109 <Sudhasunt2006@gmail.com>
2026-08-16 19:59:37 +0530

    Fix Render MySQL SSL

--- backend/config/db.js
+++ backend/config/db.js
@@ -8,7 +8,9 @@ const pool = mysql.createPool({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
 
-
+  ssl: {
+    rejectUnauthorized: false
+  },
 
   waitForConnections: true,
   connectionLimit: 10,
